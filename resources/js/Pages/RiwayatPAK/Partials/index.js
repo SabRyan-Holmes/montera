e6375a4912512de727715c16aef6222892a81ab0
Sab-Ryan-Holmes <sabrian101maulana@gmail.com>
2026-01-07 00:54:02 +0700

    cleans up some unnecessary code.

--- resources/js/Pages/RiwayatPAK/Partials/index.js
+++
@@ -1,6 +0,0 @@
-import AkumulasiTable from "./AkumulasiTable"
-import InputDataTable from "./InputDataTable"
-import KonversiTable from "./KonversiTable"
-import PAKTable from "./PAKTable"
-
-export {InputDataTable, AkumulasiTable, KonversiTable, PAKTable }
